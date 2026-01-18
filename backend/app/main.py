cf46037d0733863cf503b475145993db8ddd3577
kamran <mohammadkamranahmad786@gmail.com>
2026-01-18 16:10:31 +0530

    main upated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 from app.routers import quiz
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # replace with Netlify URL later
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
 
 app.include_router(quiz.router)
